bde8e1e820ea9093f935a826a61cb6de965f7d7d
Frandy_DLC <82332172+anfoxy-ware@users.noreply.github.com>
2026-04-02 05:57:22 +0200

    Nuevas peliculas

--- js/videos.js
+++ js/videos.js
@@ -40,7 +40,7 @@ const videos = [
             {
         categoria: "Peliculas",
         platform:  "embed",
-        videoId:   "https://ww3.gnulahd.nu/nuevo/embed.php?id=55c9ba4031f1761a45438331266dbdd0e9fd44c5",
+        videoId:   "https://bysedikamoum.com/e/dzvtnt17bugl",
         titulo:    "Geotormenta",
         duracion:  "1h 49min",
         thumb:     "assets/imagenes/Geo.jpg"
@@ -49,7 +49,7 @@ const videos = [
             {
         categoria: "Peliculas",
         platform:  "embed",
-        videoId:   "https://ww3.gnulahd.nu/nuevo/embed.php?id=525b5c58595b461b1fb729bc93882679130571ca",
+        videoId:   "https://bysedikamoum.com/e/jb91rq7gyazh",
         titulo:    "2012",
         duracion:  "2h 37min",
         thumb:     "assets/imagenes/2012.jpg"
@@ -74,16 +74,61 @@ const videos = [
     },
 
     {
-    categoria: "Peliculas",
-    platform: "embed", 
-    videoId: "https://bysedikamoum.com/e/0g7um7sosr1c",
-    titulo: "Solicitud de Amistad",
-    duracion: "1h 30min",
-    thumb: "https://pics.filmaffinity.com/Friend_Request-273484278-large.jpg"
-},
+        categoria: "Peliculas",
+        platform: "embed", 
+        videoId: "https://bysedikamoum.com/e/0g7um7sosr1c",
+        titulo: "Solicitud de Amistad",
+        duracion: "1h 30min",
+        thumb: "https://pics.filmaffinity.com/Friend_Request-273484278-large.jpg"
+    },
+
+        {
+        categoria: "Peliculas",
+        platform: "embed", 
+        videoId: "https://bysedikamoum.com/e/dhreesndgqrx",
+        titulo: "Until Dawm",
+        duracion: "1h 37min",
+        thumb: "https://m.media-amazon.com/images/M/MV5BNzg5MTNkOTQtZDU2ZC00MjM3LTk1MDAtMTYyZDFmN2Y2MTY5XkEyXkFqcGc@._V1_.jpg"
+    },
+            {
+        categoria: "Peliculas",
+        platform: "embed", 
+        videoId: "https://bysedikamoum.com/e/3zplzw2z5su2",
+        titulo: "Dejar el mundo atrás",
+        duracion: "2h 21min",
+        thumb: "https://pics.filmaffinity.com/Dejar_el_mundo_atraas-433256303-mmed.jpg"
+    },
+            {
+        categoria: "Peliculas",
+        platform: "embed", 
+        videoId: "https://bysedikamoum.com/e/dq1dl5o3voz8",
+        titulo: "Interestelar",
+        duracion: "2h 49min",
+        thumb: "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+    },
+
+    {
+        categoria: "Peliculas",
+        platform: "embed", 
+        videoId: "https://vimeos.net/embed-1ru5fc8d959w.html",
+        titulo: "Eliminar amigo",
+        duracion: "1h 22min",
+        thumb: "https://m.media-amazon.com/images/S/pv-target-images/fdb6bc2c6f0e12a81fd585d5448a57508684f0330eae1a3f3e0fa58267a1494b.jpg"
+    },
+
+    {
+        categoria: "Peliculas",
+        platform: "embed", 
+        videoId: "https://vimeos.net/embed-qhtwfjm0rs7n.html",
+        titulo: "Eliminar amigo 2",
+        duracion: "1h 37min",
+        thumb: "https://play-lh.googleusercontent.com/z4jAymVGGE3PjTpSnw-Fr2iKNebVp85zTNHlcl1Dpz7JKsazB33PIKOlQVcfFqY4E55rgaEw-cOcE72at38"
+    },
 
 
-    // ── SERIES ─────────────────────────────────────
+
+
+    /* ── SERIES ─────────────────────────────────────
     {
         categoria: "Series",
         platform:  "drive",
@@ -102,6 +147,8 @@ const videos = [
         duracion:  "10min",
         thumb:     ""
     },
+
+    */
     
 
     // 👇 Copia y pega este bloque para agregar más:
